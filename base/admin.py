d1c19f118c1585deb416c4950fae074bd570a5d0
pouya <pouya@gmail.com>
2023-11-06 08:27:59 -0800

    add admin and models

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SchoolInfo, ClassInfo
+from .models import SchoolInfo, ClassInfo, Student, StudentClass
 
 
 
@@ -11,3 +11,16 @@ class SchoolInfoAdmin(admin.ModelAdmin):
 @admin.register(ClassInfo)
 class ClassInfoAdmin(admin.ModelAdmin):
     list_display = ['name', 'studentNumber', 'grade', 'classNumber']
+
+
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'lastName', 'nationalCode', 'phoneNumber', 'fatherPhoneNumber', 'fatherName', 'homePhoneNumber', 'dateOfBirth', 'address', 'image']
+
+
+
+@admin.register(StudentClass)
+class StudentClassAdmin(admin.ModelAdmin):
+    list_display = ['student', 'clas']
--- base/models.py
+++ base/models.py
@@ -47,3 +47,34 @@ class ClassInfo(models.Model):
     classNumber = models.IntegerField(help_text='شماره کلاس را وارد نمایید')
 
 
+    def __str__(self):
+        return self.name
+
+
+
+class Student(models.Model):     
+    name = models.CharField(max_length=100, help_text='نام خود را وارد نمایید')
+    lastName = models.CharField(max_length=100, help_text='نام خانوادگی خود را وارد نمایید')
+    nationalCode = models.IntegerField(unique=True, help_text='شماره ملی خود را وارد نمایید')
+    phoneNumber = models.IntegerField(unique=True, help_text='شماره تلفن خود را وارد نمایید')
+    fatherPhoneNumber = models.IntegerField(help_text='شماره تلفن پدر را وارد نمایید')
+    fatherName = models.CharField(max_length=100, help_text='نام پدر را وارد نمایید')
+    homePhoneNumber = models.IntegerField(help_text='شماره تلفن منزل را وارد نمایید')
+    dateOfBirth = models.DateField(auto_now=False, auto_now_add=False, help_text='تاریخ تولد خود را وارد نمایید')
+    address = models.CharField(max_length=500, null=True, blank=True, help_text='آدرس منزل خود را وارد نمایید')
+    image = models.ImageField(upload_to='imageStudents', null=True, blank=True, help_text='عکس خود را وارد نمایید')
+
+
+
+    def __str__(self):
+        return f'{self.name} , {self.lastName}'
+
+
+
+
+class StudentClass(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    clas = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
+
+
+    
